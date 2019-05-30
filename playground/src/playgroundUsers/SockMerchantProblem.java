33d09baa6daa4726cdfb2c5c5caf3ec9e069c4c4
VALENTIN38 <VALENTIN38@npetro36365690.ww.omv.com>
2019-05-30 14:52:05 +0300

    -- completed sock merchant challenge

--- playground/src/playgroundUsers/SockMerchantProblem.java
+++ playground/src/playgroundUsers/SockMerchantProblem.java
@@ -1,29 +1,41 @@
 package playgroundUsers;
 
+import java.util.ArrayList;
+
 public class SockMerchantProblem {
 
 	private static int sockMerchant(int n, int ar[]) {
 		
-		int nrOfSocksPair = 0;
+		ArrayList<Integer> ar1 = new ArrayList<Integer>();
 		
 		for(int i=0;i<n;i++) {
-			for(int j=i;j<n;j++) {
-				
-				if(ar[i] == ar[j]) {
-					
-					nrOfSocksPair++;
-					break;
-					
-				}
-				
-			}
 			
+			ar1.add(ar[i]);
+			
+		}
+		
+		int nrOfSocksPair = 0;
+		
+		for(int a=0;a<ar1.size();a++) {
+			for(int j=a+1;j<ar1.size();j++) {
+				
+					if(ar1.get(a) == ar1.get(j)) {
+						
+						nrOfSocksPair++;
+						
+						ar1.remove(j);
+						break;
+						
+					}
+				
+					
+			}
 			
 		}
 		
 		
 		
-		return n;
+		return nrOfSocksPair;
 
 	}
 	
@@ -31,9 +43,11 @@ public class SockMerchantProblem {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-			int ar[] = {1,2,1,2,1,3,2};
+			int ar[] = {4,5,5,5,6,6,4,1,4,3,6,6,3,6,1,4,5,5,5};
 			int n = ar.length;
 			
+			
+			System.out.println(sockMerchant(n, ar));
 			
 			
 		
