3a3c62bb30344886a6f93c8c8537e00c11f821ce
rober <rober@DESKTOP-V0DUEJV>
2020-03-06 23:06:35 +0200

    --updated Test class

--- SolarSystem/src/solarSystem/Test.java
+++ SolarSystem/src/solarSystem/Test.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Test {
 	
@@ -12,7 +14,7 @@ public class Test {
 		
 		String path;
 		try {
-			path = new File(".").getCanonicalPath() + System.getProperty("file.separator") + "src" + System.getProperty("file.separator") + "SolarSystem" + System.getProperty("file.separator") + "Configuration.xml";
+			path = new File(".").getCanonicalPath() + System.getProperty("file.separator") + "src" + System.getProperty("file.separator") + "solarSystem" + System.getProperty("file.separator") + "Configuration.xml";
 			
 			System.out.println(path);
 			
@@ -21,6 +23,23 @@ public class Test {
 			for(CelestialBody cb : cBodies) {
 				
 				System.out.println(cb.getName());
+				
+			}
+			
+			List<CelestialBody> father = cBodies.stream().filter( p -> p.getFather().equals("none")).collect(Collectors.toList());
+			
+			for(CelestialBody celestB : father) {
+				
+				System.out.println(celestB.getName());
+				
+				for(CelestialBody cb : cBodies) {
+					
+					cb.orbiting(celestB.getxCoord(), celestB.getyCoord(), cb.getRadius(), cb.getOrbitalPeriod(), cb.getVelocity(), 1000);
+					
+					System.out.println(cb.getxCoord());
+					System.out.println(cb.getyCoord());
+					
+				}
 				
 			}
 			
